4c83e4f44ad5542660a4de8a5c05b1b01aac6432
aucatomahamil-sudo <aucatomahamil@gmail.com>
2025-12-23 08:10:53 +0800

    更新 jianting.lua

--- jianting.lua
+++ jianting.lua
@@ -1,27 +1,36 @@
--- listener.lua
-print("Listener ON")
+-- jianting.lua - 最小监听器
+print("Listener Started")
 
-for i, obj in pairs(game:GetDescendants()) do
-    if obj.ClassName == "RemoteEvent" then
-        local old = obj.FireServer
-        obj.FireServer = function(...)
-            print("EVENT " .. obj.Name)
-            return old(...)
+-- 监听远程事件
+for _, obj in pairs(game:GetDescendants()) do
+    if obj:IsA("RemoteEvent") then
+        local oldFire = obj.FireServer
+        obj.FireServer = function(self, ...)
+            print("EVENT: " .. obj.Name)
+            return oldFire(self, ...)
+        end
+    elseif obj:IsA("RemoteFunction") then
+        local oldInvoke = obj.InvokeServer
+        obj.InvokeServer = function(self, ...)
+            print("FUNCTION: " .. obj.Name)
+            return oldInvoke(self, ...)
         end
     end
 end
 
-local p = game.Players.LocalPlayer
-if p then
-    for i, child in pairs(p:GetDescendants()) do
-        if child.ClassName == "NumberValue" then
-            child.Changed:Connect(function()
-                print("VAL " .. child.Name .. " " .. child.Value)
+-- 监听玩家数值
+local player = game.Players.LocalPlayer
+if player then
+    for _, child in pairs(player:GetDescendants()) do
+        if child:IsA("NumberValue") then
+            child:GetPropertyChangedSignal("Value"):Connect(function()
+                print("VALUE: " .. child.Name .. " = " .. child.Value)
             end)
         end
     end
 end
 
+-- 保持运行
 while true do
-    wait(1)
+    wait(5)
 end
